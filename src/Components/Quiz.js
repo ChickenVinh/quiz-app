ac7510e5612ed8a5c0ee6ba703302d059fea8fc8
Vinh <nguyenvinh1997@yahoo.de>
2020-05-24 05:13:36 +0700

    loading screen

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,43 +1,57 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
+import React, {useEffect} from 'react'
+import { Row, Col, Button, Container } from 'react-bootstrap'
 import { fetchQuestions } from './Actions'
-import {useDispatch} from 'react-redux'
+import { useDispatch , useSelector } from 'react-redux'
+import DotLoader from 'react-spinners/DotLoader'
 
 const Quiz = () => {
     const dispatch = useDispatch()
-    dispatch(fetchQuestions())
-    return(
-        <>
-        <Row className = 'mt-5 mb-3'>
+    const state = useSelector(state => state)
+
+    useEffect(() => {
+        dispatch(fetchQuestions())
+    }, [])
+
+    if(state.loading === true){
+        return(
+            <Container className = 'mt-5 d-flex justify-content-center'>
+                <DotLoader
+                    color = {'DodgerBlue'}
+                />
+            </Container>
+        )
+    }else if(state.loading ===false){
+        return(
+            <Container>
+            <Row className = 'mt-5 mb-3'>
+                <Col className = 'd-flex justify-content-center'>
+                    <h3>Questions</h3>
+                </Col>
+            </Row>
+            <Row className = 'mb-3'>
+                <Col className = 'd-flex justify-content-center'>
+                    <Button className = 'btn btn-secondary'>Answer 1</Button>
+                </Col>
+                <Col className = 'd-flex justify-content-center'>
+                    <Button className = 'btn btn-secondary'>Answer 2</Button>
+                </Col>
+            </Row>
+            <Row className = 'mb-3'>
+                <Col className = 'd-flex justify-content-center'>
+                    <Button className = 'btn btn-secondary'>Answer 1</Button>
+                </Col>
+                <Col className = 'd-flex justify-content-center'>
+                    <Button className = 'btn btn-secondary'>Answer 2</Button>
+                </Col>
+            </Row>
+            <Row>
             <Col className = 'd-flex justify-content-center'>
-                <h3>Question</h3>
+                <Button>Next</Button>
             </Col>
-        </Row>
-        <Row className = 'mb-3'>
-            <Col className = 'd-flex justify-content-center'>
-                <Button className = 'btn btn-secondary'>Answer 1</Button>
-            </Col>
-            <Col className = 'd-flex justify-content-center'>
-                <Button className = 'btn btn-secondary'>Answer 2</Button>
-            </Col>
-        </Row>
-        <Row className = 'mb-3'>
-            <Col className = 'd-flex justify-content-center'>
-                <Button className = 'btn btn-secondary'>Answer 1</Button>
-            </Col>
-            <Col className = 'd-flex justify-content-center'>
-                <Button className = 'btn btn-secondary'>Answer 2</Button>
-            </Col>
-        </Row>
-        <Row>
-        <Col className = 'd-flex justify-content-center'>
-            <Button>Next</Button>
-        </Col>
-        </Row>
-        </>
-    )
+            </Row>
+            </Container>
+        )
+    }
 }
 
 export default Quiz
